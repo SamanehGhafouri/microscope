c82b2ea30b66ea4f4a970779d3e61d3dc36cc533
samaneh ghafouri <samanehghafouri@samanehs-MacBook-Pro.local>
2020-06-15 16:28:29 -0400

    [Creating-Posts: Added a submit post page and linked to it in the header]

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,16 @@
+import './post_submit.html';
+
+//Creating posts
+Template.postSubmit.events({
+    'submit form': function (e) {
+        e.preventDefault();
+
+        let post = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val()
+        };
+        post._id = Posts.insert(post);
+        Router.go('postPage', post);
+
+    }
+});
