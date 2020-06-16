2702a15bbdfa82bdcb2f213ae0d2da17e40d8741
samaneh ghafouri <samanehghafouri@samanehs-MacBook-Pro.local>
2020-06-16 16:44:23 -0400

    [Creating-post: Use a method to submit the post]

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,6 +1,6 @@
 import './post_submit.html';
 import '../includes/access_denied.js';
-import {Posts} from "../../../lib/collections/Posts.js";
+
 
 //Creating posts: insert some input to the submit table and to be added to our list in the first page
 Template.postSubmit.events({
@@ -12,10 +12,23 @@ Template.postSubmit.events({
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val()
         };
+
+        //Meteor method call: better abstraction and security
+        //always have two arguments, error and result
+        Meteor.call('postInsert', post, function (error, result) {
+            //display the error to the user and abort
+            if (error)
+                return alert(error.reason);
+
+            Router.go('postPage', {_id: result._id});
+
+        });
+
+        //     REMOVE THIS PLEASE!!!!!!!!!!!!!!!!!
         //generate _id for the object that has been inserted into the database
-        post._id = Posts.insert(post);
-        // construct a URL to browse to
-        Router.go('postPage', post);
+        // post._id = Posts.insert(post);
+        // // construct a URL to browse to
+        // Router.go('postPage', post);
 
     }
 });
