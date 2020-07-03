939e48bd74e0f0f1ab5563e1ab69707d6a65283b
samaneh ghafouri <samanehghafouri@samanehs-MacBook-Pro.local>
2020-07-03 16:42:34 -0400

    [Voting: Added active classes to the header.]

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -1,2 +1,20 @@
 import './header.html';
 import '../../templates/application/layout.js';
+
+//A Better header
+//show the reactivity whenever we vote or post new that post jump up and down in real time
+Template.header.helpers({
+    activeRouteClass: function () {
+
+        let args = Array.prototype.slice.call(arguments, 0);
+        args.pop();
+
+        let active = _.any(args, function (name) {
+            return Router.current() && Router.current().route.getName() === name
+
+        });
+
+        return active && 'active';
+
+    }
+})
